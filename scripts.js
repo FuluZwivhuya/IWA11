22cdd46d5f5ed9c08680e781c363545847d035d4
FuluZwivhuya <148289949+FuluZwivhuya@users.noreply.github.com>
2024-02-16 00:50:03 +0200

    not done

---
+++ scripts.js
@@ -0,0 +1,40 @@
+// script.js
+
+const root1 = document.querySelector('[data-key="order1"]');
+const biscuits1 = document.querySelector('[data-biscuits="10"]');
+const donuts1 = document.querySelector('[data-donuts="13"]');
+const pancakes1 = document.querySelector('[data-pancakes="0"]');
+const status1 = document.querySelector('[data-delivered="false"]');
+console.log(biscuits1)
+console.log (root1)
+
+const root2 = document.querySelector('[data-key="order2"]'); 
+const biscuits2 = document.querySelector('[data-biscuits="5"]');
+const donuts2 = document.querySelector('[data-donuts="0"]');
+const pancakes2 = document.querySelector('[data-pancakes="2"]');
+const status2 = document.querySelector('[data-delivered="false"]');
+
+const root3 = document.querySelector('[data-key="order3"]');
+const biscuits3 = document.querySelector('[data-biscuits="12"]');
+const donuts3 = document.querySelector('[data-donuts="11" ]');
+const pancakes3 = document.querySelector('[data-pancakes="15"]');
+const status3 = document.querySelector('[data-delivered="true]');
+
+//console.log(root3)
+let myCount = document.getElementsByClassName("count");
+console.log(myCount)
+
+/*biscuits = biscuits1.getAttribute('biscuit1');
+donuts1   = 1-root.donuts,
+pancakes1 = 1-root.pancakes,
+status1   = 1-root.status ? Delivered : Pending
+
+biscuits2 = 2-root.biscuits,
+donuts2   = 2-root.donuts,
+pancakes2 = 2-root.pancakes,
+status2   = 2-root.status ? Delivered : Pending
+
+biscuits3 = 3-root.biscuits,
+donuts3   = 3-root.donuts,
+pancakes3 = 3-root.pancakes,
+status3   = 3-root.status ? Delivered : Pending*/
