811dcbea4807f542b63e96ac455e05700a28fa8a
FuluZwivhuya <148289949+FuluZwivhuya@users.noreply.github.com>
2024-02-17 01:48:40 +0200

    used an unothodox method

--- scripts.js
+++ scripts.js
@@ -1,12 +1,17 @@
-// script.js
+/*script.js
+let r;//root
+let b; //biscuits
+let d; // donuts
+let p; // pancakes
+let s; //status
+*/
+
 
 const root1 = document.querySelector('[data-key="order1"]');
 const biscuits1 = document.querySelector('[data-biscuits="10"]');
 const donuts1 = document.querySelector('[data-donuts="13"]');
 const pancakes1 = document.querySelector('[data-pancakes="0"]');
-const status1 = document.querySelector('[data-delivered="false"]');
-console.log(biscuits1)
-console.log (root1)
+const status1 = document.querySelector('[data-delivered="false"]')//console.log (biscuits1)
 
 const root2 = document.querySelector('[data-key="order2"]'); 
 const biscuits2 = document.querySelector('[data-biscuits="5"]');
@@ -23,6 +28,22 @@ const status3 = document.querySelector('[data-delivered="true]');
 //console.log(root3)
 let myCount = document.getElementsByClassName("count");
 console.log(myCount)
+myCount[0].textContent = '10';
+myCount[1].textContent ='13';
+myCount[2].textContent ='0';
+myCount[3].textContent = '5';
+myCount[4].textContent ='0';
+myCount[5].textContent ='2';
+myCount[6].textContent = '12';
+myCount[7].textContent = '11';
+myCount[8].textContent = '15';
+
+let myStatus = document.getElementsByClassName("status");
+console.log(myStatus)
+myStatus[0].textContent = 'false';
+myStatus[1].textContent = 'false';
+myStatus[2].textContent = 'true';
+
 
 /*biscuits = biscuits1.getAttribute('biscuit1');
 donuts1   = 1-root.donuts,
