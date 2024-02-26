31f8e718e3fd1b133220bf7364e6581ef76192fc
FuluZwivhuya <148289949+FuluZwivhuya@users.noreply.github.com>
2024-02-26 21:05:59 +0200

    suggested solution by coach

--- scripts.js
+++ scripts.js
@@ -1,20 +1,19 @@
-
-let sections = document.getElementsByTagName('section');
+let sections = document.getElementsByTagName("section");
 console.log(sections);
 
 const root1 = sections[0].querySelector("dl").getAttribute("data-key");
-const biscuits1 = sections[0].querySelector("dl").getAttribute("data-biscuits")
+const biscuits1 = document.querySelector('[data-biscuits="order1"]'); // suggested change, I tried it before and it didn't work out well for me, still not working
 const donuts1 = sections[0].querySelector("dl").getAttribute("data-donuts");
 const pancakes1 = sections[0].querySelector("dl").getAttribute("data-pancakes");
 const status1 = sections[0].querySelector("dl").getAttribute("data-delivered");
-console.log (biscuits1);
+console.log(biscuits1);
 
 const root2 = sections[1].querySelector("dl").getAttribute("data-key");
 const biscuits2 = sections[1].querySelector("dl").getAttribute("data-biscuits");
 const donuts2 = sections[1].querySelector("dl").getAttribute("data-donuts");
 const pancakes2 = sections[1].querySelector("dl").getAttribute("data-pancakes");
 const status2 = sections[1].querySelector("dl").getAttribute("data-delivered");
-console.log(biscuits2)
+console.log(biscuits2);
 
 const root3 = sections[2].querySelector("dl").getAttribute("data-key");
 const biscuits3 = sections[2].querySelector("dl").getAttribute("data-biscuits");
@@ -24,19 +23,19 @@ const status3 = sections[2].querySelector("dl").getAttribute("data-delivered");
 
 let myCount = document.getElementsByClassName("count");
 
-console.log(myCount)
+console.log(myCount);
 myCount[0].textContent = biscuits1;
-myCount[1].textContent =donuts1;
-myCount[2].textContent =pancakes1;
+myCount[1].textContent = donuts1;
+myCount[2].textContent = pancakes1;
 myCount[3].textContent = biscuits2;
-myCount[4].textContent =donuts2;
-myCount[5].textContent =pancakes2;
+myCount[4].textContent = donuts2;
+myCount[5].textContent = pancakes2;
 myCount[6].textContent = biscuits3;
 myCount[7].textContent = donuts3;
 myCount[8].textContent = pancakes3;
 
 let myStatus = document.getElementsByClassName("status");
-console.log(myStatus)
+console.log(myStatus);
 myStatus[0].textContent = status1;
 myStatus[1].textContent = status2;
 myStatus[2].textContent = status3;
